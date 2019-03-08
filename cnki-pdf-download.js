484f5b84023e34724b9905295396aaab4ba7ed21
Tom <hexuxin@foxmail.com>
2019-03-09 00:21:50 +0800

    Fixed some bugs.

--- cnki-pdf-download.js
+++ cnki-pdf-download.js
@@ -1,21 +1,24 @@
 // ==UserScript==
 // @name         CNKI PDF Download
-// @namespace    http://tomcat.one/
+// @namespace    https://tomcat.one/
 // @version      0.2
 // @description  Provide PDF Download button for master's thesis on cnki.net
 // @author       tomacat
 // @match        http://kns.cnki.net/KCMS/detail/detail.aspx?*
 // @grant        none
 // @license      GNU General Public License v3.0 or later
-// @supportURL   https://github.com/troutperch/cnki-pdf-download.js/
+// @supportURL   https://github.com/troutperch/cnki-pdf-download.js
 // ==/UserScript==
 
 window.onload = function() {
     var cdjDownload = $('.icon-dlGreen').attr('href');
     var pdfDownload = cdjDownload.replace("dflag=nhdown", "dflag=pdfdown");
     var overseaDownload = pdfDownload.replace("/kns/download.aspx", "http://gb.oversea.cnki.net/kcms/download.aspx");
-    var appendHtml = "<a href='"+overseaDownload+"' class='icon icon-dlpdf'>PDF 下载</a>";
-    $(".dllink").append(appendHtml);
-    $(".rootw .wxToolbar .dllink").css("margin-right","200px");
-    $(".wxToolbar .total").css("padding-left","150px");
+    var anchorLink = "<a href='"+overseaDownload+"' class='icon icon-dlpdf'>PDF下载</a>";
+    $(".dllink").append(anchorLink);
+    $(".rootw .wxToolbar .dllink").css("margin-right", "200px");
+    $(".rootw .wxToolbar .infotxt").css("width", "160px");
+    $('.icon-dlGreen').addClass("icon-dlcaj");
+    $('.icon-dlGreen').removeClass("icon-dlGreen");
+
 };
