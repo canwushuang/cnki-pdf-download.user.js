d93c073a8f7bb4d2d3986051d020a75335dd90e3
Tom <hexuxin@foxmail.com>
2019-03-08 23:15:50 +0800

    Update version 0.2:  - Support floating bar.  - Modify CSS style.

--- cnki-pdf-download.js
+++ cnki-pdf-download.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         CNKI PDF Download
 // @namespace    http://tomcat.one/
-// @version      0.1
+// @version      0.2
 // @description  Provide PDF Download button for master's thesis on cnki.net
-// @author       tomacat
+// @author       tomcat
 // @match        http://kns.cnki.net/KCMS/detail/detail.aspx?*
 // @grant        none
 // @license      GNU General Public License v3.0 or later
@@ -15,5 +15,7 @@ window.onload = function() {
     var pdfDownload = cdjDownload.replace("dflag=nhdown", "dflag=pdfdown");
     var overseaDownload = pdfDownload.replace("/kns/download.aspx", "http://gb.oversea.cnki.net/kcms/download.aspx");
     var appendHtml = "<a href='"+overseaDownload+"' class='icon icon-dlpdf'>PDF 下载</a>";
-    $("#DownLoadParts").append(appendHtml);
+    $(".dllink").append(appendHtml);
+    $(".rootw .wxToolbar .dllink").css("margin-right","200px");
+    $(".wxToolbar .total").css("padding-left","150px");
 };
