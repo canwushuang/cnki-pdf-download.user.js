e85d512441121568b4456df8ce2497cb476c1323
Tom <hexuxin@foxmail.com>
2019-10-19 16:09:11 +0800

    Fix some bugs: - Check if the PDF link exist. - Fix the bug on non-master's thesis page.

--- cnki-pdf-download.user.js
+++ cnki-pdf-download.user.js
@@ -12,14 +12,16 @@
 // ==/UserScript==
 
 (function() {
-    // Replace the link.
-    var cdjDownload = $('.icon-dlGreen')[0].href;
-    var pdfDownload = cdjDownload.replace("dflag=nhdown", "dflag=pdfdown");
-    var overseaDownload = pdfDownload.replace("//kns.cnki.net/kns/download.aspx", "//gb.oversea.cnki.net/kcms/download.aspx");
-    var anchorLink = "<a href='" + overseaDownload + "' class='icon icon-dlpdf' rel='noreferrer' target='_blank'>PDF下载</a>";
-    $(".dllink").append(anchorLink);
-    $(".rootw .wxToolbar .dllink").css("margin-right", "200px");
-    $(".rootw .wxToolbar .infotxt").css("width", "160px");
+    if ($('#pdfDown').length == 0){
+        // Replace the link.
+        var cdjDownload = $('.icon-dlGreen')[0].href;
+        var pdfDownload = cdjDownload.replace("dflag=nhdown", "dflag=pdfdown");
+        var overseaDownload = pdfDownload.replace("https://kns.cnki.net/kns/download.aspx", "http://gb.oversea.cnki.net/kcms/download.aspx");
+        var anchorLink = "<a href='" + overseaDownload + "' class='icon icon-dlpdf' rel='noreferrer' target='_blank'>PDF下载</a>";
+        $(".dllink").append(anchorLink);
+        $(".rootw .wxToolbar .dllink").css("margin-right", "200px");
+        $(".rootw .wxToolbar .infotxt").css("width", "160px");
+    }
     // Unbind onclick event.
     $('.icon-dlGreen,.icon-dlBlue').prop("onclick", null).off("click");
 }).apply(null);
