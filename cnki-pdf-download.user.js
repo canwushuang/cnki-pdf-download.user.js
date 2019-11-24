ca031c05daace349c67d9dfd8c92a8471ebd60d6
Tom <hexuxin@foxmail.com>
2019-11-24 18:28:32 +0800

    Update version 1.3

--- cnki-pdf-download.user.js
+++ cnki-pdf-download.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cnki-pdf-download.user.js
 // @namespace    https://tomcat.one/
-// @version      1.2
+// @version      1.3
 // @description  Provide PDF Download button for master's thesis on cnki.net
 // @author       tomacat
 // @match        https://kns.cnki.net/KCMS/detail/detail.aspx?*
@@ -15,29 +15,28 @@
 // @run-at       document-end
 // ==/UserScript==
 
-var srName = "%ccnki-pdf-download.user.js:%c";
-var srVer = "version 1.2";
-var srCol = "color:blue;";
-console.info(srName + srVer, srCol);
-console.info(srName + "\nTitle: " + $('.title')[0].text + "\nAuthor: " + $('.author')[0].text + "\nOrganization: " + $('.orgn')[0].text + "\nLocation: " + window.location, srCol);
-
 (function() {
+    var srName = "cnki-pdf-download.user.js: ";
+    var srVer = "version 1.3";
+    console.info(srName + srVer);
+    console.info(srName + "\nTitle: " + $('.title')[0].text + "\nAuthor: " + $('.author')[0].text + "\nOrganization: " + $('.orgn')[0].text + "\nLocation: " + window.location);
+
     if ($('#pdfDown').length == 0){
-        console.info(srName + "The PDF button is not existing.", srCol);
+        console.info(srName + "The PDF button is not existing.");
         // Replace the link.
         var cdjDownload = $('.icon-dlGreen')[0].href;
         var pdfDownload = cdjDownload.replace("dflag=nhdown", "dflag=pdfdown");
         var overseaDownload = pdfDownload.replace("https://kns.cnki.net/kns/download.aspx", "http://gb.oversea.cnki.net/kcms/download.aspx");
-        console.info(srName + "\nCDJ: " + cdjDownload + "\nPDF: " + overseaDownload, srCol);
+        console.info(srName + "\nCDJ: " + cdjDownload + "\nPDF: " + overseaDownload);
         var anchorLink = "<a href='" + overseaDownload + "' class='icon icon-dlpdf' rel='noreferrer' target='_blank'>PDF下载</a>";
         $(".dllink").append(anchorLink);
         if($('.icon-dlpdf').length >= 1){
-            console.info(srName + "A PDF button has been added.", srCol);
+            console.info(srName + "A PDF button has been added.");
         }
         $(".rootw .wxToolbar .dllink").css("margin-right", "200px");
         $(".rootw .wxToolbar .infotxt").css("width", "160px");
     } else {
-        console.info(srName + "The PDF button is existing.", srCol);
+        console.info(srName + "The PDF button is existing.");
     }
     // Unbind onclick event.
     $('.icon-dlGreen,.icon-dlBlue').prop("onclick", null).off("click");
